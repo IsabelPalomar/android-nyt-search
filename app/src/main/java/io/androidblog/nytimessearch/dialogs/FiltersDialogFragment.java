aadc92329e4ddd59e5efa0d0e701a4ee7fdfbfc1
Isabel Palomar <isabel.palomarb@gmail.com>
2016-07-31 20:15:50 -0600

    Filters dialog layout

--- app/src/main/java/io/androidblog/nytimessearch/dialogs/FiltersDialogFragment.java
+++ app/src/main/java/io/androidblog/nytimessearch/dialogs/FiltersDialogFragment.java
@@ -1,23 +1,32 @@
 package io.androidblog.nytimessearch.dialogs;
 
+import android.app.DatePickerDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
-import android.view.LayoutInflater;
+import android.support.v7.app.AlertDialog;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.WindowManager;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Locale;
 
 import io.androidblog.nytimessearch.R;
+import io.androidblog.nytimessearch.activities.SearchActivity;
 
 public class FiltersDialogFragment extends DialogFragment {
-    private EditText mEditText;
+    View view;
+    Resources res;
+    TextView etBeginDate;
 
     public FiltersDialogFragment() {
-        // Empty constructor is required for DialogFragment
-        // Make sure not to add arguments to the constructor
-        // Use `newInstance` instead as shown below
     }
 
     public static FiltersDialogFragment newInstance(String title) {
@@ -29,21 +38,63 @@ public class FiltersDialogFragment extends DialogFragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_edit_name, container);
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        res = getResources();
+
+        view = getActivity().getLayoutInflater().inflate(R.layout.fragment_edit_name, null);
+        etBeginDate = (EditText) view.findViewById(R.id.etBeginDate);
+
+        etBeginDate.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                // TODO Auto-generated method stub
+                //To show current date in the datepicker
+                final Calendar mcurrentDate = Calendar.getInstance();
+                int mYear = mcurrentDate.get(Calendar.YEAR);
+
+                int mMonth = mcurrentDate.get(Calendar.MONTH);
+                int mDay = mcurrentDate.get(Calendar.DAY_OF_MONTH);
+
+
+                DatePickerDialog mDatePicker = new DatePickerDialog(getContext(), new DatePickerDialog.OnDateSetListener() {
+                    public void onDateSet(DatePicker datepicker, int selectedyear, int selectedmonth, int selectedday) {
+
+                        String myFormat = "MM/dd/yy"; //In which you need put here
+                        SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
+
+                        etBeginDate.setText(sdf.format(mcurrentDate.getTime()));
+
+                    }
+                }, mYear, mMonth, mDay);
+                mDatePicker.setTitle("Select date");
+                mDatePicker.show();
+            }
+        });
+
+        builder.setView(view)
+                .setTitle(res.getString(R.string.app_message_filter_dialog_title))
+                .setPositiveButton(res.getString(R.string.app_message_apply),
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                            }
+                        })
+                .setNegativeButton(res.getString(R.string.app_message_cancel),
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int whichButton) {
+                                getDialog().dismiss();
+                            }
+                        });
+
+        return builder.create();
     }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        // Get field from view
-        mEditText = (EditText) view.findViewById(R.id.txt_your_name);
-        // Fetch arguments from bundle and set title
-        String title = getArguments().getString("title", "Enter Name");
-        getDialog().setTitle(title);
-        // Show soft keyboard automatically and request focus to field
-        mEditText.requestFocus();
         getDialog().getWindow().setSoftInputMode(
                 WindowManager.LayoutParams.SOFT_INPUT_STATE_VISIBLE);
     }
